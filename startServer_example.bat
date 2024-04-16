3d6acbc8503a520f469197b5ef35ad09bcf7e4d1
sjstein <s.joshua.stein@gmail.com>
2024-04-16 16:30:06 -0500

    cleaning up some docs

--- startServer_example.bat
+++ startServer_example.bat
@@ -2,5 +2,6 @@
 :: rename this file to "startServer.bat" (remove the ""_example") and
 :: make sure this file is in the same directory as your Run8 executable
 @ECHO OFF
-cd C:\Run8Studios\Run8 Train Simulator V3
+C:
+cd "C:\Run8Studios\Run8 Train Simulator V3"
 start "" "Run-8 Train Simulator V3.exe" -ServerConfigFile ServerConfig.xml
