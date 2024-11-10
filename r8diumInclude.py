45f5c0a4f3f6b71f4139cf2a3cb8112c4528ce2e
Killerp51 <jaredsturdivant@gmail.com>
2024-11-10 11:41:59 -0600

    pull updates

--- r8diumInclude.py
+++ r8diumInclude.py
@@ -55,9 +55,6 @@ try:
     # Discord bot status
     BOT_STATUS = config['discord']['bot_status']
 
-    # Discord role assignment
-    DISCORD_ROLE = config['discord']['discord_role']
-
     # Discord channels
     CH_ADMIN = config['discord']['ch_admin']
     CH_LOG = config['discord']['ch_log']
